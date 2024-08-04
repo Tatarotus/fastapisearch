a466261f528decc219acb69939ac48746acbebf5
Samuel Baruch <shamuelbaruch@gmail.com>
2024-08-04 16:01:30 -0300

    add hono and setup for cf worker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,29 @@
+"use client";
+import { useState, useEffect } from "react";
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  const [input, setInput] = useState<string>("");
+  const [searchResults, setSearchResults] = useState<{
+    results: string[];
+    duration: number;
+  }>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!input) return setSearchResults(undefined);
+
+      const res = await fetch(`/api/search?q=${input}`);
+    };
+    fetchData();
+  }, [input]);
+
+  return (
+    <>
+      <input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        type="text"
+      />
+    </>
+  );
 }
